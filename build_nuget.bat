15b0e3a4a0e07ff05d8b3b6c048d5fe8fd3210a9
Cam Sinclair <csinkers@gmail.com>
2020-08-30 22:23:06 -0400

    Update build script

--- build_nuget.bat
+++ build_nuget.bat
@@ -1,14 +1,9 @@
 @echo off
 dotnet clean -c Debug
 dotnet clean -c Release
-rm -rf .\bin
+rd /S /Q .\bin
 dotnet build -c Release
 dotnet pack -c Release 
-:: dotnet nuget add source https://nuget.pkg.github.com/csinkers/index.json -n github -u username -p token
-:: dotnet nuget push "bin/Release/AdlMidi.NET.VERSION.nupkg" --source "github"
-:: dotnet nuget push "bin/Release/AdlMidi.NET.VERSION.nupkg" --source nuget.org -k NUGET_KEY
-:: dotnet nuget remove source github
-
 :: dotnet nuget add source https://nuget.pkg.github.com/csinkers/index.json -n github -u username -p token
 :: dotnet nuget push "src/bin/Release/SerdesNet.VERSION.nupkg" --source "github"
 :: dotnet nuget push "src/bin/Release/SerdesNet.VERSION.nupkg" --source nuget.org -k NUGET_KEY
